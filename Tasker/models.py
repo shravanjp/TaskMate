5fd9d2c66c4dcfbd12499fb8aa6e018198fe4e2d
ShravanJP <shravanjp8910@gmail.com>
2023-04-06 14:08:43 +0530

    feat: add ToDoList  model

--- Tasker/models.py
+++ Tasker/models.py
@@ -12,3 +12,17 @@ class Category(models.Model):
     def __str__(self):
         return self.name
 
+class ToDoList(models.Model):
+    title = models.CharField(max_length=250)
+    content = models.TextField(blank=True)
+    created_time = models.DateField(default=timezone.now().strftime("%d-%m-%y")) 
+    due_time = models.DateField(default=timezone.now().strftime("%d-%m-%y")) 
+    category = models.ForeignKey(Category,default="general")
+
+    class Meta:
+        ordering = ["-created_time"]
+
+    def __str__(self):
+        return self.title
+
+
