e11bf5bb890770651b3b4448cfffee5956532d76
ShravanJP <shravanjp8910@gmail.com>
2023-04-06 14:06:04 +0530

    feat: add Category model

--- Tasker/models.py
+++ Tasker/models.py
@@ -1,3 +1,23 @@
 from django.db import models
-
+from django.utils import timezone
 # Create your models here.
+
+class Category(models.Model):
+    name : models.CharField(max_length = 100)
+
+    class Meta:
+        verbose_name = 'Category'
+        verbose_name_plural = "Categories"
+
+    def __str__(self):
+        return self.name
+
+class ToDoList(models.Model):
+    title = models.CharField(max_length=250)
+    content = models.TextField(blank=True)
+    created_time = models.DateField(default=timezone.now().strftime("%d-%m-%y")) 
+    due_time = models.DateField(default=timezone.now().strftime("%d-%m-%y")) 
+    category = models.ForeignKey(Category,default="general")
+
+    class Meta:
+        ordering = ["-created_time"]
