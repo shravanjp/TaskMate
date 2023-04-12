dc1b6d56aa56bd8d4d1c6c47cfa7bc73ae9e6909
ShravanJP <shravanjp8910@gmail.com>
2023-04-12 17:31:28 +0530

    fix: add css mimetype. If your system has dreamviewer installed then it could cause some problem. So to avoid add css mimetype

--- TaskMate/settings.py
+++ TaskMate/settings.py
@@ -11,7 +11,9 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
-import os
+import os,mimetypes
+
+mimetypes.add_type("text/css",".css",True)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
